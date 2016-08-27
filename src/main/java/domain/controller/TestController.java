0938d898c64a91b59e354d5acc0f45d2fea1deca
slavpetroff <slavpetroff@gmail.com>
2016-08-27 16:30:34 +0300

    Added test controller and created css rules

--- src/main/java/domain/controller/TestController.java
+++ src/main/java/domain/controller/TestController.java
@@ -2,6 +2,7 @@ package domain.controller;
 
 import domain.models.service.IssueService;
 import domain.models.service.ProjectService;
+import domain.models.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,18 +17,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     private final IssueService issueService;
-
+    private final TypeService typeService;
     private final ProjectService projectService;
 
     @Autowired
-    public TestController(IssueService issueService, ProjectService projectService) {
+    public TestController(IssueService issueService,
+                          ProjectService projectService,
+                          TypeService typeService) {
         this.issueService = issueService;
         this.projectService = projectService;
+        this.typeService = typeService;
     }
 
     @GetMapping
     public String test(Model model) {
-//        model.addAttribute("issues", this.projectService.findOne(3L).getIssues());
+        model.addAttribute("issues", this.projectService.findOne(3L).getIssues());
+        model.addAttribute("types", this.typeService.findAll());
         return "test";
     }
 }
---
+++ src/main/resources/static/issues.js
@@ -0,0 +1,12 @@
+/**
+ * Created by slavpetroff on 8/26/16.
+ */
+"use strict";
+
+$(document).ready(function () {
+    var type = $(".type");
+
+    var dropDiv = document.createElement("div");
+    var dropDown = document.createElement("")
+
+});
